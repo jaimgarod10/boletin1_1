dc1ae757cd85878b1781b0c5ebc62e2cff60ed4f
Jaime García O Dowd <jgarciaodowd@victoria130.ad.danielcastelao.org>
2019-10-08 09:39:28 +0200

    Inicio

---
+++ boletín1/Boletín1_1.java
@@ -0,0 +1,16 @@
+
+package boletín1;
+
+
+public class Boletín1 {
+
+    
+    public static void main(String[] args) {
+        // calcular el area de ubn triángulo
+        float base= 4 ; 
+        float altura =3;
+        float area= base*altura/2;
+        System.out.println("area =" + area);
+    }
+    
+}
